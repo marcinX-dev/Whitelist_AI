ae55c6c4df266abfec2e6c5105430de155e810d9
marcinX-dev <marcin.kozlowskii95@gmail.com>
2026-01-16 14:45:31 +0100

    Refactor match_categories function to include company profile and product description; update database queries to retrieve product description and categories whitelist

--- ai_solution.py
+++ ai_solution.py
@@ -16,7 +16,7 @@ class CategoryMatches(BaseModel):
     matches: List[CategoryCandidate] = []
 
 
-def match_categories(description: str, categories: list[tuple[int, str]]) -> CategoryMatches:
+def match_categories(company_profile: str, product_description: str, categories: list[tuple[int, str]], whitelist: list[tuple[int, str]]) -> CategoryMatches:
     openai_client = OpenAI(api_key=OPENAI_API_KEY)
     confidence_threshold = 0.7
     system_prompt = (
@@ -26,29 +26,44 @@ def match_categories(description: str, categories: list[tuple[int, str]]) -> Cat
         "Jeśli żadna nie pasuje → zwróć pustą listę matches: [].\n"
         "Nie wymyślaj nowych kategorii.\n"
         "Nie zgaduj — jeśli niska pewność, nie dodawaj kategorii."
+        "Jeśli dostaniesz listę 'JUŻ WYBRANE (wzorzec)' — wzoruj się na niej i dobieraj spójne kategorie."
     )
 
     categories_text = "\n".join([f"{cid}: {cpl}" for cid, cpl in categories])
-
+    
+    whitelist_text = "\n".join([f"{cid}: {cpl}" for cid, cpl in whitelist])
+    if whitelist:
+        system_prompt += (
+            "\n\nJUŻ WYBRANE (wzorzec):\n"
+            f"{whitelist_text}\n"
+            "Dobierz do nich podobne kategorie."
+        )
+    
     response = openai_client.responses.parse(
         model="gpt-5.1-2025-11-13",
         input=[
             {"role": "system", "content": system_prompt},
-            {"role": "user", "content": f"OPIS:\n{description}\n\nKATEGORIE:\n{categories_text}"},
+            {"role": "user", "content": f"""OPIS:\n
+                                        Profil firmy: {company_profile}\n
+                                        Opis produktu: {product_description}\n\n
+                                        KATEGORIE:\n{categories_text}"""},
         ],
         service_tier="flex",
         text_format=CategoryMatches,
     )
-
+    
     result: CategoryMatches = response.output_parsed
 
     # zabezpieczenie: wyrzucamy ID spoza listy
     valid = {cid: cpl for cid, cpl in categories}
+    whitelist_ids = {cid for cid, _ in whitelist}
+    
     result.matches = [
-        # CategoryCandidate(id=m.id, pl=valid[m.id], confidence=m.confidence)
         m.id
         for m in result.matches
-        if m.id in valid and m.confidence >= confidence_threshold
+        if m.id in valid 
+        and m.confidence >= confidence_threshold
+        and m.id not in whitelist_ids
     ]
 
     return result.matches
--- app.py
+++ app.py
@@ -8,15 +8,16 @@ def get_company_profile(user_id: int):
     with conn.cursor() as cursor:
         cursor.execute(
             """
-            SELECT company_profile
-            FROM user_config
-            WHERE user_id = %s;
+            SELECT company_profile, product_description
+            FROM user_config uc
+            LEFT JOIN users u ON uc.user_id = u.id
+            WHERE uc.user_id = %s;
             """,
             (user_id,),
         )
         row = cursor.fetchone()
     conn.close()
-    return row["company_profile"] if row else None
+    return (row["company_profile"], row["product_description"]) if row else (None, None)
 
 
 def get_categories():
@@ -32,13 +33,16 @@ def get_categories():
     conn.close()
     return rows
 
-def get_users():
+def get_users_and_rule_list(categories):
     conn = get_postgres_connection('system')
     with conn.cursor() as cursor:
         cursor.execute(
             """
-            SELECT id 
+            SELECT 
+                u.id,
+                COALESCE(rl.categories_whitelist, '{}'::int[]) AS categories_whitelist
             FROM users u
+            LEFT JOIN rule_list rl ON u.id = rl.user_id
             LEFT JOIN user_config uc ON u.id = uc.user_id
             WHERE u.is_active = TRUE 
                 AND whitelist_ai = TRUE
@@ -46,6 +50,8 @@ def get_users():
             """)
         rows = cursor.fetchall()
     conn.close()
+    for row in rows:
+        row['categories_whitelist'] = [(cat['id'], cat['pl']) for cat in categories if cat['id'] in row['categories_whitelist']]
     return rows
 
 
@@ -54,20 +60,36 @@ if __name__ == "__main__":
     
     try:
         while True:
-            categories = get_categories()
-            users = get_users()
+            all_categories = get_categories()
+            users = get_users_and_rule_list(all_categories)
             
             for user in users:
                 user_id = user['id']
                 print(f"Processing user_id: {user_id}")
-                company_profile = get_company_profile(user_id)
+                company_profile, product_description = get_company_profile(user_id)
                 print(f"Company profile: {company_profile}")
+                print(f"Product description: {product_description}")
                 
                 new_categories = set()
                 
+                # ID kategorii już przypisanych temu userowi
+                whitelist_ids = {cid for cid, _ in user['categories_whitelist']}
+                
+                #  wywal z batcha kategorie, które już są w whitelist
+                categories = [row for row in all_categories if row['id'] not in whitelist_ids]
+                
                 for i in tqdm(range(0, len(categories), batch_size)):
-                    batch = categories[i:i + batch_size]
-                    matched_categories = match_categories(company_profile, batch)
+                    batch_rows = categories[i:i + batch_size]
+                    
+                    batch = [(row['id'], row['pl']) for row in batch_rows]
+                    
+                    matched_categories = match_categories(
+                        company_profile, 
+                        product_description,
+                        batch, 
+                        user['categories_whitelist']
+                        )
+                    
                     new_categories.update(matched_categories)
                 
                 pp(f"Matched categories for user {user_id}: {new_categories}")    
